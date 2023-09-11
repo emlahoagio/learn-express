67c46524e1f920643d852c8b0be541bc2d9593f4
QuanDo <emlahoagio0610@gmail.com>
2023-09-11 15:44:20 +0700

    Install husky, prittier, and lint-staged

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
-const app = express();
-const port = 3000;
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
 
-const route = require('./routes');
+const route = require('./routes')
 
 // Config path for static file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Config middleware
-        app.use(express.urlencoded({ extended: true }));
-app.use(        express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 // XMLHttpRequest, fetch, axios, ...
 
 // HTTP logger
@@ -24,13 +24,13 @@ app.engine(
     handlebars.engine({
         extname: '.hbs',
     }),
-);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
 
 // Routes init
-            route(app);
+route(app)
 
-            app.listen(port, () => {
-                console.log(`Example app listening on port ${port}`);
-            });
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
