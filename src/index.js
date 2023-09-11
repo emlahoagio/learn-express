74a99e6512b3239c00e92a6c9d6271832e324a08
QuanDo <emlahoagio0610@gmail.com>
2023-09-11 10:43:42 +0700

    Refactor code : add route and controller follow MVC model

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+const route = require("./routes");
 
 // Config path for static file
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
+
+// Config middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// XMLHttpRequest, fetch, axios, ...
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('home')
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
